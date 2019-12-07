f5cd5c4d31196be949b475f9f7b3a2d9ce4fee00
Jen <jen.rodasky@gmail.com>
2019-12-07 12:02:31 -0600

    completed assignment

--- assignment01/assignment1.js
+++ assignment01/assignment1.js
@@ -1,4 +1,6 @@
-$(document).ready();{
+$(document).ready(function($){
     $('#first-name').html('Jen');
     $('#last-name').html('Cink');
-};
+    $('.mcc').css({'color': 'blue', 'font-style': 'italic'});
+    $('#hobby').
+});
